cb54358708339d78779f08e23c3033c4e0f2c75f
Pawel Wujczyk <pwujczyk@hotmail.com>
2025-03-16 16:57:26 +0100

    change connection string

--- ProductivityTools.Feedback.Api.DatabaseMigrations/Program.cs
+++ ProductivityTools.Feedback.Api.DatabaseMigrations/Program.cs
@@ -8,7 +8,7 @@ namespace ProductivityTools.Feedback.DatabaseMigrations
     {
         static void Main(string[] args)
         {
-            string serverName = ".\\SQL2019";// ProductivityTools.MasterConfiguration.MConfiguration.Configuration["ServerName"];
+            string serverName = ".\\SQL2022";// ProductivityTools.MasterConfiguration.MConfiguration.Configuration["ServerName"];
             string dbName = "PTTeamManagement";// ProductivityTools.MasterConfiguration.MConfiguration.Configuration["DatabaseName"];
             DBUp dBUp = new DBUp("tm");
             Assembly assembly = Assembly.GetExecutingAssembly();
